eb0068b90f0f33100644d21d1c7c5c046b1068f4
mbeas2021 <maicojobs@gmail.com>
2026-07-04 03:00:08 -0300

    first commit

---
+++ controladores/enviar.php
@@ -0,0 +1,13 @@
+<?php
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+$destino = "tucorreo@dominio.com";
+$asunto = "Nuevo mensaje desde el formulario";
+
+$contenido = "Nombre: $nombre\nCorreo: $email\nMensaje:\n$mensaje";
+
+mail($destino, $asunto, $contenido);
+header("Location: index.html");
+?>
